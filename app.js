0fe44dafa48e4ea51906e4220eba31f0c016d159
natalielyn <nlmaxwell@gmail.com>
2019-12-17 14:08:17 -0800

    added randomizer function for color selection

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ let colors = [
 ];
 
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
+let messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,25 @@ for (let i = 0; i < squares.length; i++){
     let clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
-        alert('Correct!');
+        messageDisplay.textContent = 'Correct!'
+        changeColors(clickedColor);
     } else {
-        alert('Wrong!!');
+        this.style.backgroundColor = '#232323';
+        messageDisplay.textContent = 'Try Again';
     }
     });
+}
+
+
+function changeColors(color){
+    //loop all squares
+for (let i = 0; i < squares.length; i++){
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+}
+}
+
+function pickColor(){
+    let random = Math.floor(Math.random() * colors.length)
+    return colors[random];
 }
