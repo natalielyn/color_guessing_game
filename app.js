4e7cfabf2816811a4d94edac53cf4fea2a69ab16
natalielyn <nlmaxwell@gmail.com>
2019-12-17 13:52:50 -0800

    added event listener to squares, and alerts to tell user if right or wrong

--- app.js
+++ app.js
@@ -14,5 +14,18 @@ let colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for (let i = 0; i < squares.length; i++){
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i]
+
+    //add click listeners to squares
+    squares[i].addEventListener('click', function(){
+    //grab color of picked square
+    let clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if (clickedColor === pickedColor) {
+        alert('Correct!');
+    } else {
+        alert('Wrong!!');
+    }
+    });
 }
